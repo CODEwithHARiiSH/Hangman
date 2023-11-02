fcac2956ab352714de1edf3f8aac65e56810826e
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-02 14:29:32 +0530

    Red:Implements function for word length greater thet 5, test fails

--- test_hangman.py
+++ test_hangman.py
@@ -5,6 +5,9 @@ def test_get_word_lowercase():
     
 def test_random_word_no_punctuation():
     assert hangman.get_word().isalpha()
+    
+def test_random_words_minlen5():
+    assert len(hangman.get_word()) >= 5
 
 
  
