ff971a3c4ca89321e504b7aecf1d3c36ccc52cd5
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-02 14:25:01 +0530

    Green:Implements function for alphabet only, test success

--- hangman.py
+++ hangman.py
@@ -1,2 +1,15 @@
-def get_word():
-    return "eliphant"
+import random
+
+
+def get_word(wordlist="/usr/share/dict/words"):
+    good_words=[]
+    with open(wordlist) as f:
+        words=[x.strip() for x in f]
+        for word in words:
+            if not word.islower():
+                continue
+            elif not word.isalpha():
+                continue
+            good_words.append(word)
+    return random.choice(good_words)
+        
--- test_hangman.py
+++ test_hangman.py
@@ -4,7 +4,7 @@ def test_get_word_lowercase():
     assert hangman.get_word().islower()
     
 def test_random_word_no_punctuation():
-    assert hangman.get_words().isalpha()
+    assert hangman.get_word().isalpha()
 
 
  
