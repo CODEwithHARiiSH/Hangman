0c6ebe9555f8942abff11cad8af8c6961e9e4847
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-02 14:13:34 +0530

    Red:Implements function for words with alphabet only, test fails

--- test_hangman.py
+++ test_hangman.py
@@ -2,3 +2,9 @@ import hangman
 
 def test_get_word_lowercase():
     assert hangman.get_word().islower()
+    
+def test_random_word_no_punctuation():
+    assert hangman.get_words().isalpha()
+
+
+ 
