dce99d589c1e6ba15c31d8d35accc3e559296c0a
CODEwithHARiiSH <harishthampyhat@gmail.com>
2023-11-02 14:41:08 +0530

    Implements test reliable.
    We had used words from dictionary for testing. Now we are using a sample word file. So the test is more reliable now.

--- test_hangman.py
+++ test_hangman.py
@@ -1,13 +1,30 @@
+import os
 import hangman
 
 def test_get_word_lowercase():
-    assert hangman.get_word().islower()
+    fname = "/tmp/sample_wordlist"
+    with open(fname, "w") as f:
+        f.writelines(["Grape\n", "apple\n", "Mango\n"])
+    for _ in range(100):
+        assert hangman.get_word(fname) == "apple"
+    os.unlink(fname)
     
 def test_random_word_no_punctuation():
-    assert hangman.get_word().isalpha()
+    fname = "/tmp/sample_wordlist"
+    with open(fname, "w") as f:
+        f.writelines(["grape's\n", "apple\n", "mango'j\n"])
+    for _ in range(100):
+        assert hangman.get_word(fname) == "apple"
+    os.unlink(fname)
+ 
     
 def test_random_words_minlen5():
-    assert len(hangman.get_word()) >= 5
+    fname = "/tmp/sample_wordlist"
+    with open(fname, "w") as f:
+        f.writelines(["god\n", "apple\n", "dad\n"])
+    for _ in range(100):
+        assert hangman.get_word(fname) == "apple"
+    os.unlink(fname)
 
 
  
